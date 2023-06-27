1af5c686dea9ccb5425392543da63e0374f9ba17
maita.galang <maita.galang@gmail.com>
2023-06-27 09:00:42 +0800

    debug 2307

--- Accounting/Pay/bir2307.php
+++ Accounting/Pay/bir2307.php
@@ -359,14 +359,14 @@
 			From paybill_t A 
 			left join
 				(
-					Select compcode, ctranno, cewtcode, sum(namount) as namount, sum(ndue) as ndue, sum(newtamt) as newtamt
+					Select compcode, ctranno, cewtcode, sum(nnet) as namount, sum(ndue) as ndue, sum(newtamt) as newtamt
 					From apv_d
 					Group by compcode, ctranno, cewtcode
 					
 					UNION ALL 
 					
 					Select G.compcode, G.ctranno, G.cewtcode, sum(G.ncredit) as namount, 
-					CASE WHEN G.cacctno not in ('".implode("','",$disreg)."') AND I.ccategory='LIABILITIES' THEN SUM(G.ncredit) ELSE 0 END as ndue,
+					CASE WHEN G.cacctno not in ('".implode("','",$disreg)."') THEN SUM(G.ndebit) ELSE 0 END as ndue,
 					CASE WHEN G.cacctno = '".$disregEWT."' THEN SUM(G.ncredit) ELSE 0 END as newtamt
 					From apv_t G 
 					left join apv H on G.compcode=H.compcode and G.ctranno=H.ctranno
