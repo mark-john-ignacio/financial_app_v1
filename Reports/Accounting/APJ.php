d92b5d424a51699ec19420ce40cd34b02b1ad098
maita.galang <maita.galang@gmail.com>
2023-06-29 14:40:14 +0800

    reports update

--- Reports/Accounting/APJ.php
+++ Reports/Accounting/APJ.php
@@ -44,7 +44,7 @@ $varmsg = "";
 </center>
 
 <hr>
-<table width="40%" border="0" align="center" cellpadding="2px">
+<table width="50%" border="0" align="center" cellpadding="2px">
   <tr>
     <th width="80">Acct Code</th>
     <th>Account Title</th>
