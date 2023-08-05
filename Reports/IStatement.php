71191dfbc93e0c1438484dcacfd09dec1a7f113c
maita.galang <maita.galang@gmail.com>
2023-08-05 08:03:15 +0800

    monthly IS

--- Reports/IStatement.php
+++ Reports/IStatement.php
@@ -45,26 +45,19 @@ $last_day_this_month  = date("Y-m-t", strtotime($first_day_this_month));
             <span class="glyphicon glyphicon-search"></span> View Report
           </button>
         </td>
-        <td width="150" style="padding-left:10px"><b>Date Range: </b></td>
+        <td width="150" style="padding-left:10px"><b>Report Type: </b></td>
         <td style="padding:2px">
           <div class="col-xs-12 nopadding">
             <div class="col-xs-3 nopadding">
               
-              <input type='text' class="datepick form-control input-sm" id="date1" name="date1" value="<?php echo date("m/01/Y"); ?>"  />
+              <select id="selrpt" name="selrpt" class="form-control input-sm selectpicker"  tabindex="4">
+                <option value="Accounting/IncomeStatement">Summary</option>   
+                <option value="Accounting/IncomeStatement_Monthly">Monthly</option>               
+              </select>
               
-              </div>
+            </div>
             
-            <div class="col-xs-2 nopadding" style="vertical-align:bottom;" align="center">
-              <label style="padding:1px;">TO</label>
-              </div>
-            
-            <div class="col-xs-3 nopadding">
-              
-              <input type='text' class="datepick form-control input-sm" id="date2" name="date2" value="<?php echo date("m/d/Y"); ?>" />
-              
-              </div>
-            
-            </div>   
+          </div>   
         </td>
 
       </tr>
@@ -74,6 +67,47 @@ $last_day_this_month  = date("Y-m-t", strtotime($first_day_this_month));
             <i class="fa fa-file-excel-o"></i> To Excel
           </button>
         </td>
+        
+        <td width="150" style="padding-left:10px"><div id="dtelabel"><b>Date Range: </b></div></td>
+        <td style="padding:2px">
+          <div id="dterange">
+            <div class="col-xs-12 nopadding">
+              <div class="col-xs-3 nopadding">             
+                <input type='text' class="datepick form-control input-sm" id="date1" name="date1" value="<?php echo date("m/d/Y"); ?>" />              
+              </div>
+              
+              <div class="col-xs-2 nopadding" style="vertical-align:bottom;" align="center">
+                <label style="padding:1px;">TO</label>
+              </div>
+              
+              <div class="col-xs-3 nopadding">              
+                <input type='text' class="datepick form-control input-sm" id="date2" name="date2" value="<?php echo date("m/d/Y"); ?>" />              
+              </div>           
+            </div>   
+          </div>
+
+          <div id="dtemonth" style="display:none">
+              <div class="col-xs-12 nopadding">
+                <div class="col-xs-3 nopadding">          
+                  <select id="selyr" name="selyr" class="form-control input-sm selectpicker"  tabindex="4">
+                    <?php
+                      $yrnow = date("Y");
+                      $yrstart = 2023;
+                      for($i=$yrnow; $i>=$yrstart ; $i--){
+                    ?>
+                      <option value="<?=$i?>"><?=$i?></option>     
+                    <?php
+                      }
+                    ?>
+                  </select>           
+                </div>
+              </div>
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td valign="top" width="50" style="padding:2px">&nbsp;</td>
         <td width="150" style="padding-left:10px"><b>Provision for IT: </b></td>
         <td style="padding:2px">
           <div class="col-xs-12 nopadding">
@@ -126,15 +160,27 @@ $last_day_this_month  = date("Y-m-t", strtotime($first_day_this_month));
       $(this).select();
     });
 
+    $("#selrpt").on("change", function(){
+      if($(this).val()=="Accounting/IncomeStatement_Monthly"){
+        $("#dtelabel").html("<b>Year: </b>");
+        $("#dterange").hide();
+        $("#dtemonth").show();
+      }else{
+        $("#dtelabel").html("<b>Date Range: </b>");
+        $("#dterange").show();
+        $("#dtemonth").hide();
+      }
+    });
+
     $('#btnView').on("click", function(){
-      $dval = $("#seltyp").val();
-      $('#frmrep').attr("action", "Accounting/IncomeStatement.php");
+      $dval = $("#selrpt").val();
+      $('#frmrep').attr("action", $dval+".php");
       $('#frmrep').submit();
     });
 
     $('#btnexcel').on("click", function(){
-      $dval = $("#seltyp").val();
-      $('#frmrep').attr("action", "Accounting/IncomeStatement_xls.php");
+      $dval = $("#selrpt").val();
+      $('#frmrep').attr("action", $dval+"_xls.php");
       $('#frmrep').submit();
     });
 
