ded77ac8c9765b9b4875f3cd8d207942edd09cd1
maita.galang <maita.galang@gmail.com>
2023-09-26 16:09:09 +0800

    debug

--- MasterFiles/th_accounts.php
+++ MasterFiles/th_accounts.php
@@ -9,8 +9,6 @@ require_once "../Connection/connection_string.php";
 	$company = '001';
 	
 	$result = mysqli_query ($con, "SELECT cacctid, cacctno, cacctdesc, IFNULL(nbalance,0) as nbalance FROM accounts WHERE compcode='$company' and (cacctdesc like '%".$_GET['query']."%' OR cacctid like '%".$_GET['query']."%') and ctype='Details'"); 
-
-	 echo "SELECT cacctid, cacctno, cacctdesc, IFNULL(nbalance,0) as nbalance FROM accounts WHERE compcode='$company' and (cacctdesc like '%".$_GET['query']."%' OR cacctid like '%".$_GET['query']."%') and ctype='Details'";
 	
 	//$json2 = array();
 	//$json = [];
