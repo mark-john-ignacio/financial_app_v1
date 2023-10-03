2e82c8fa0ebafe4120c9a64cd6f3e7e503900750
Clyde Limbaco <kuraido.eiron@gmail.com>
2023-10-03 17:08:51 +0800

    Print Module Update

--- Sales/Sales/SI_print.php
+++ Sales/Sales/SI_print.php
@@ -68,7 +68,7 @@ if (mysqli_num_rows($sqlhead)!=0) {
 <head>
 </head>
 
-<body style="padding-top:0.8in" onLoad="window.print()">
+<body style="padding-top:.7in" onLoad="window.print()">
 
 <table width="100%" border="0" cellpadding="1" style="border-collapse:collapse;" id="tblMain">
   <tr>
@@ -80,7 +80,7 @@ if (mysqli_num_rows($sqlhead)!=0) {
 
       <table width="100%" border="0" cellpadding="2" style=" margin-top: 0.18in !important">
         <tr><td style="padding-left: 1.0in;"> <?=$CustName?> </td></tr>
-        <tr><td style="padding-left: 1.0in; padding-top: px"><?=$cTin?></td></tr>
+        <tr><td style="padding-left: 1.0in; padding-top: 5px"><?=$cTin?></td></tr>
         <tr><td style="padding-left: 1.0in; padding-top: 5px"><?=$Adds?> </td></tr>       
         </tr>
       </table>
