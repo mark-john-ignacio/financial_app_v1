6e56bede61e409a34ef283ea707af259ec12f232
maita.galang <maita.galang@gmail.com>
2023-10-04 08:07:37 +0800

    update reset

--- MasterFiles/Users/users_reset.php
+++ MasterFiles/Users/users_reset.php
@@ -9,18 +9,18 @@ function better_crypt($input, $rounds = 12) {
 
  
 	$cUserID = $_REQUEST['empid'];
-	$cPass = 'Password';
+	$cPass = 'Password123';
 	
 	$password_hash = better_crypt($cPass);
 		 
-	$sql = "update users set `password`= '$password_hash' where Userid='$cUserID'";
+	$sql = "update users set `password`= '$password_hash', cstatus='Active' where Userid='$cUserID'";
 
 		if (!mysqli_query($con, $sql)) {
 			printf("Errormessage: %s\n", mysqli_error($con));
 		} 
 
 echo '<script language="javascript">
-alert("User\'s Password is reset to: password")
+alert("User\'s Password is reset to: Password123")
 location.href="users.php?f="
 </script>';
 
