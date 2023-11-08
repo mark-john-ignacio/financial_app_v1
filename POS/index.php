8a0a65da5243247110ac539ca9c8f2c86f7af7a5
Clyde Limbaco <kuraido.eiron@gmail.com>
2023-11-08 14:24:52 +0800

    POS Upadte

--- POS/index.php
+++ POS/index.php
@@ -1292,7 +1292,6 @@
                         } else {
                             alert(res.msg)
                         }
-                        
                     },
                     error: function(res){
                         console.log(res)
