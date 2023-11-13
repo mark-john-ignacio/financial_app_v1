ed98cfe03489a1af5e2e1f71328b1f2cb5292025
Clyde Limbaco <kuraido.eiron@gmail.com>
2023-11-13 09:01:12 +0800

    Update code for Mass Uploading

--- MassUpload/th_saveSD.php
+++ MassUpload/th_saveSD.php
@@ -4,70 +4,17 @@
         session_start();
     }
     include("../Connection/connection_string.php");
+    require_once ("../Model/helper.php");
     $company = $_SESSION['companyid'];
-    $proceed = true;
     $duplicate = false;
     $isFinished = false;
 
     $dmonth = date('m');
     $dyear = date('y');
 
-    $excel_data = [];
-    if (isset($_FILES['excel_file']) && !empty($_FILES['excel_file'])) {
-        $file = $_FILES['excel_file'];
+    $excel_data = ExcelRead($_FILES);
 
-        if ($file['error'] === 0) {
-            $fileExt = pathinfo($file['name'], PATHINFO_EXTENSION);
-
-            if (in_array($fileExt, ['xlsx', 'xls'])) {
-                $uploadDir = './';
-                $uploadedFile = $uploadDir . $file['name'];
-                move_uploaded_file($file['tmp_name'], $uploadedFile);
-
-                require '../vendor2/autoload.php';
-
-                $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($uploadedFile);
-                $worksheet = $spreadsheet->getActiveSheet();
-
-                foreach ($worksheet->getRowIterator() as $row) {
-                    $cellIterator = $row->getCellIterator();
-                    $rowdata = [];
-
-                    $hasNonNullValue = false;
-
-                    foreach ($cellIterator as $cell) {
-                        $cellValue = trim($cell->getValue());
-
-                        if (!is_null($cellValue) and !empty($cellValue)) {
-                            $hasNonNullValue = true;
-                        }
-
-                        // Check if the cell can be converted to a date
-                        if (\PhpOffice\PhpSpreadsheet\Shared\Date::isDateTime($cell)) {
-                            $date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($cellValue);
-                            $formattedDate = $date->format('Y-m-d'); // Adjust the format as needed
-                            $rowdata[] = $formattedDate;
-                        } else {
-                            $rowdata[] = trim($cellValue);
-                        }
-                    }
-
-                    if ($hasNonNullValue) {
-                        // echo json_encode($rowdata);
-                        array_push($excel_data, $rowdata);
-                    }
-                }
-
-                unlink($uploadedFile);
-            } else {
-                $proceed = false;
-            }
-        } else {
-            $proceed = false;
-        }
-    }
-
-    if($proceed){
+    if(!empty($excel_data)){
 
         for($i = 1; $i < sizeof($excel_data); $i++){
             $chkSales = mysqli_query($con,"select * from discounts where compcode='$company' and YEAR(ddate) = YEAR(CURDATE()) Order By ctranno desc LIMIT 1");
