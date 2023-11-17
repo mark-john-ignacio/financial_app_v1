9dc83416a6fcb821ee3d7f060d2e8cab8a9ec24b
maita.galang <maita.galang@gmail.com>
2023-11-17 16:23:39 +0800

    update

--- Sales/DR_BPTI/DR_print.php
+++ Sales/DR_BPTI/DR_print.php
@@ -47,7 +47,9 @@ if (mysqli_num_rows($sqlhead)!=0) {
     $Adds = $row['chouseno']." ". $row['ccity']." ". $row['cstate'];
     $cTin = $row['ctin'];
 
-		//$SalesType = $row['csalestype'];
+		$cAPCDR = $row['crefapcdr'];
+    $cAPCORD = $row['crefapcord'];
+
 		$Gross = $row['ngross'];
     $cTerms = $row['cterms'];
 		
@@ -61,70 +63,179 @@ if (mysqli_num_rows($sqlhead)!=0) {
 <!DOCTYPE html>
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="../../css/cssmed.css">
-
 <head>
+  <style type="text/css">
+    body{
+      font-family: Arial;
+      font-size: 14px;
+    }
+
+		.form-container{
+				position: relative;
+				text-align: center;
+				color: #000;
+				font-weight: bold;
+				text-transform: uppercase;
+				width: 8.5in;
+				height: 7in;
+		}
+
+		.delTo{
+			position: absolute;
+			top: 158px;
+			left: 125px;
+			width: 400px;
+			height:  15px;  
+      text-align: left; 
+     /* border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .address{
+			position: absolute;
+			top: 178px;
+			left: 125px;
+			width: 400px;
+			height:  15px;  
+      text-align: left; 
+     /* border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .tin{
+			position: absolute;
+			top: 198px;
+			left: 125px;
+			width: 400px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .date{
+			position: absolute;
+			top: 160px;
+			left: 650px;
+			width: 135px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .terms{
+			position: absolute;
+			top: 180px;
+			left: 650px;
+			width: 135px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .refdr{
+			position: absolute;
+			top: 200px;
+			left: 650px;
+			width: 135px;
+			height:  15px;  
+      text-align: left; 
+     /* border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		} 
+
+    .reforder{
+			position: absolute;
+			top: 260px;
+			left: 50px;
+			width: 135px;
+			height:  15px;  
+      text-align: left; 
+     /* border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}   
+
+    .partDesc{
+			position: absolute;
+			left: 195px;
+			width: 195px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .partNo{
+			position: absolute;
+			left: 396px;
+			width: 155px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+    .partQty{
+			position: absolute;
+			left: 560px;
+			width: 80px;
+			height:  15px;  
+      text-align: left; 
+      /*border: 1px solid #000; 
+			letter-spacing: 11px;
+			border: 1px solid #000;*/
+		}
+
+  </style>
 </head>
 
-<body style="padding-top:.79in" onLoad="window.print()">
+<body onLoad="window.print()">
 
-<table width="100%" border="0" cellpadding="1" style="border-collapse:collapse;" id="tblMain">
-  <tr>
-    <td colspan="2" style="padding-right: 0.25in;" align="right"><font size="3"><b><?php echo $csalesno;?></b></font></td>
-  </tr>
+  <div class="form-container">
+    <!--<img src="drform.png" width="100%">-->
 
-  <tr>
-    <td VALIGN="TOP">
+    <div class="delTo"><?=$CustName?></div>
+    <div class="date"><?=date_format(date_create($Date), "M d, Y")?></div>
+    <div class="address"><?=$Adds?></div>
+    <div class="terms"><?=$cTerms?></div>
+    <div class="tin"><?=$cTin?></div>
+    <div class="refdr"><?=$cAPCDR?></div>
 
-      <table width="100%" border="0" cellpadding="2" style=" margin-top: .16in !important">
-        <tr><td style="padding-left: 1.2in;"> <?=$CustName?> </td></tr>
-        <tr><td style="padding-left: 1.2in"><?=$cTin?></td></tr>
-        <tr><td style="padding-left: 1.2in"><?=$Adds?> </td></tr>       
-        </tr>
-      </table>
+    <div class="reforder"><?=$cAPCORD?></div>
 
-    </td>
-    <td style="width: 2.7in" VALIGN="TOP"> 
-      <table width="100%" border="0" >
-        <tr><td style="padding-right: 0.3in;" align="right"> <?=date_format(date_create($Date), "M d, Y")?> </td></tr>
-        <tr><td style="padding-right: 0.3in;" align="right"> &nbsp; </td></tr>
-        <tr><td style="padding-right: 0.3in;" align="right"> <?=$cTerms?> </td></tr>
-        <tr><td style="padding-right: 0.3in;" align="right"> &nbsp; </td></tr>
-      </table>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="3">&nbsp;</td>
-  </tr>
-  <tr>
-    <td colspan="3" style="padding-left: 0.3in; padding-top: 30px;">
-    
-    <table width="100%" border="0" cellpadding="2">
-      <?php 
-		$sqlbody = mysqli_query($con,"select a.*,b.citemdesc from dr_t a left join items b on a.citemno=b.cpartno where a.compcode='$company' and a.ctranno = '$csalesno'");
+    <?php 
+      $sqlbody = mysqli_query($con,"select a.*,b.citemdesc from dr_t a left join items b on a.citemno=b.cpartno where a.compcode='$company' and a.ctranno = '$csalesno'");
 
-		if (mysqli_num_rows($sqlbody)!=0) { 
-		$cntr = 0;
-		$totnqty = 0;
-		while($rowbody = mysqli_fetch_array($sqlbody, MYSQLI_ASSOC)){
-		 $cntr = $cntr + 1;
-						
-	?>
-      
-      <tr>
-        <td style="width: 0.7in"><?php echo number_format($rowbody['nqty']);?></td> 
-        <td style="width: 0.8in"><?php echo $rowbody['cunit'];?></td> 
-        <td><?php echo $rowbody['citemno'];?></td>
-        <td style="text-overflow: ellipsis; width: 5in"><?php echo $rowbody['citemdesc'];?></td>
-               
-      </tr>
-  <?php
-    }
-  }
-  ?>
-    </table></td>
-  </tr>
-</table>
+      if (mysqli_num_rows($sqlbody)!=0) { 
+        $cntr = 0;
+        $totnqty = 0;
+
+        $deftop = 221;
+        while($rowbody = mysqli_fetch_array($sqlbody, MYSQLI_ASSOC)){
+          $cntr = $cntr + 1;
+          $deftop = $deftop + 39;
+              
+	  ?>
+          <div class="partDesc"  style="top: <?=$deftop?>px !important"><?php echo $rowbody['citemdesc'];?></div> 
+          <div class="partNo" style="top: <?=$deftop?>px !important"><?php echo $rowbody['citemno'];?></div>
+          <div class="partQty" style="top: <?=$deftop?>px !important"><?php echo number_format($rowbody['nqty']);?>&nbsp;<?php echo $rowbody['cunit'];?></div>
+    <?php
+        }
+      }
+    ?>
+  </div>
+
 
 </body>
 </html>
