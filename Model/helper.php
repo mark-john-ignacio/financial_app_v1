46a510779c545f1f3ac838b8959e626d37c340e6
Mhaitz - Dev <maita.galang@gmail.com>
2024-01-26 14:48:56 +0800

    encryption

--- Model/helper.php
+++ Model/helper.php
@@ -715,3 +715,42 @@
             ];
         }
     }
+
+    function MyEnc($simple_string, $key) {
+        global $con;       
+       
+        // Store the cipher method
+        $ciphering = "AES-128-CTR";
+        
+        // Use OpenSSl Encryption method
+        $iv_length = openssl_cipher_iv_length($ciphering);
+        $options = 0;
+        
+        // Non-NULL Initialization Vector for encryption
+        $encryption_iv = '1234567891011121';
+        
+        // Store the encryption key
+        $encryption_key = $key;
+        
+        // Use openssl_encrypt() function to encrypt the data
+        $encryption = openssl_encrypt($simple_string, $ciphering, $encryption_key, $options, $encryption_iv);
+        
+        // Display the encrypted string    
+        return $encryption;
+    }
+
+    function MyDec($encryption, $key) {
+
+        $ciphering = "AES-128-CTR";
+        $decryption_iv = '1234567891011121';
+        $options = 0;
+        
+        // Store the decryption key
+        $decryption_key = $key;
+        
+        // Use openssl_decrypt() function to decrypt the data
+        $decryption=openssl_decrypt ($encryption, $ciphering, $decryption_key, $options, $decryption_iv);
+        
+        // Display the decrypted string
+        return $decryption;
+    }
