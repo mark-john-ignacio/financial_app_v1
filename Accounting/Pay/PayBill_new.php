9f48436cf7016f60834452d17dbbdfefb75b1503
Mhaitz - Dev <maita.galang@gmail.com>
2024-03-02 10:26:43 +0800

    updates

--- Accounting/Pay/PayBill_new.php
+++ Accounting/Pay/PayBill_new.php
@@ -1460,7 +1460,7 @@
 			$("#MyTable > tbody > tr").each(function(index) {  
 				$chkacdsc = $(this).find('input[name="cacctdesc"]').val();
 				$chkacno = $(this).find('input[name="cacctno"]').val();
-				$chkaval = $(this).find('input[type=hidden][name="nAmount"]').val();
+				$chkaval = $(this).find('input[name="nApplied"]').val();
 
 				if($chkacdsc=="" || $chkacno=="" || $chkaval=="" || parseFloat($chkaval)==0){
 					$xx = index + 1;
