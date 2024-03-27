3b0187ff351039121fc6c28acd366e03df52b41a
Maita <maita.galang@gmail.com>
2024-03-27 13:45:03 +0800

    update

--- Purchases/th_product.php
+++ Purchases/th_product.php
@@ -7,7 +7,7 @@ require_once "../Connection/connection_string.php";
 	
 	$company = $_SESSION['companyid'];
 	
-	$sql = "select A.*, B.cacctno, B.cacctid, B.cacctdesc from items A left join accounts B on A.compcode=B.compcode and A.cacctcodewrr=B.cacctno where A.compcode='$company' and A.citemdesc LIKE '%".$_REQUEST['query']."%' and A.cstatus='ACTIVE'";
+	$sql = "select A.*, B.cacctno, B.cacctid, B.cacctdesc from items A left join accounts B on A.compcode=B.compcode and A.cacctcodewrr=B.cacctno where A.compcode='$company' and (A.citemdesc LIKE '%".$_REQUEST['query']."%' OR A.cpartno LIKE '%".$_REQUEST['query']."%') and A.cstatus='ACTIVE'";
 
 	$rsd = mysqli_query($con,$sql);
 	while($rs = mysqli_fetch_array($rsd, MYSQLI_ASSOC)) {
