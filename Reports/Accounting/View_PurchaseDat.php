f9a993259b565f3551fac8ca4f5b0f005181c69a
Maita <maita.galang@gmail.com>
2024-04-17 12:59:42 +0800

    debug

--- Reports/Accounting/View_PurchaseDat.php
+++ Reports/Accounting/View_PurchaseDat.php
@@ -100,6 +100,9 @@
                 </tr>
                 <?php 
                     $sql = "SELECT A.*, B.chouseno, B.ccity, B.cstate, B.ccountry, B.ctin, B.cname FROM apv A left join suppliers B on A.compcode=B.compcode and A.ccode=B.ccode WHERE A.compcode = '$company_code' AND A.ctranno in ('".implode("','",$allapvpaid)."') AND (A.lapproved = 1 AND A.lvoid = 0)";
+
+                    echo $sql;
+                    
                     $query = mysqli_query($con, $sql);
                     if(mysqli_num_rows($query) != 0){
                         
