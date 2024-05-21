1670000b624c6552291144e0372872065465f0ea
Maita <maita.galang@gmail.com>
2024-05-21 14:15:47 +0800

    updates

--- MRP/JO/JO_edit.php
+++ MRP/JO/JO_edit.php
@@ -1202,10 +1202,9 @@
 					$("#AlertMsg").html("");
 					$("#AlertModal").modal('hide');
 
-					//$("#myprintframe").attr("src","../../Inventory/Transfers/<?//=$prntnme['INVTRANS_REQUEST']?>?id="+data.trim()+"&n=1");
-					//$("#PrintModal").modal('show');
-
-					
+					$("#myprintframe").attr("src","../../Inventory/Transfers/<?=$prntnme['INVTRANS_REQUEST']?>?id="+data.trim()+"&n=1");
+					$("#PrintModal").modal('show');
+				
 				}
 			}
 		});
