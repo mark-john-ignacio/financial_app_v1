0ef8606f1fa345d7172e0678940bbbb1fc948f1c
Maita <maita.galang@gmail.com>
2024-06-13 14:44:55 +0800

    update

--- Reports/Inventory/InvSummaryWh.php
+++ Reports/Inventory/InvSummaryWh.php
@@ -118,13 +118,13 @@
 <table width="100%" border="0" align="center" cellpadding="3">
   <tr>
 		<?php
-		$varincnt = 1;
+		$varincnt = 2;
 		$varoutcnt = 1;
 
 			if($whse_req==$whseRR){
 				$varincnt++;
 		?>
-    <td>PU - Purchases</td>
+    	<td>PU - Purchases</td>
 		<?php
 			} 
 			
@@ -135,6 +135,7 @@
 		<?php
 			} 
 		?>
+		<td>PROD - Production Outputs</td> 
 		<td>IT - Inventory Transfer</td> 
 		<?php		
 			if($whse_req==$whseFG){
@@ -161,11 +162,11 @@
     <th rowspan="2" style="text-align:center; border-right:1px solid">UOM</th>
     <th rowspan="2" style="text-align:center; border-right:1px solid">Beg</th>
     <th colspan="<?=$varincnt?>" style="text-align:center; border-right:1px solid">Inventory In</th>
-		<th rowspan="2" style="text-align:center; border-right:1px solid">Total Available</th>
+	<th rowspan="2" style="text-align:center; border-right:1px solid">Total Available</th>
     <th colspan="<?=$varoutcnt?>" style="text-align:center; border-right:1px solid">Inventory Out</th>
     <th rowspan="2" style="text-align:center; border-right:1px solid">Theo End</th>
-		<th rowspan="2" style="text-align:center; border-right:1px solid">Actual Count</th>
-		<th rowspan="2" style="text-align:center; border-right:1px solid">Variance</th>
+	<th rowspan="2" style="text-align:center; border-right:1px solid">Actual Count</th>
+	<th rowspan="2" style="text-align:center; border-right:1px solid">Variance</th>
     <!--<th rowspan="2" style="text-align:center; border-right:1px solid">Ave Cost/Unit</th>
     <th rowspan="2" style="text-align:center; border-right:1px solid">Ave Retail/Unit</th>-->
   </tr>
@@ -182,6 +183,7 @@
 		<?php
 			}
 		?>
+		<th style="text-align:center; border-right:1px solid">PROD</th>
 		<th style="text-align:center; border-right:1px solid">IT</th>
 		<?php
 			if($whse_req==$whseFG){
@@ -256,6 +258,17 @@
 			<?php
 				}
 			?>
+			<td align="center" style="text-align: center; border-right:1px solid">
+				<?php
+					foreach($arrothers as $rsx){
+						if($rsx['ctype']=="INVCNT" && $rsx['citemno']==$rxrow['citemno']){
+							echo number_format($rsx['nqtyin']);
+							$totIn = $totIn + floatval($rsx['nqtyin']);
+							break;
+						}
+					}
+				?>
+			</td> 
 			<td align="center" style="text-align: center; border-right:1px solid">
 				<?php
 					foreach($arrothers as $rsx){
