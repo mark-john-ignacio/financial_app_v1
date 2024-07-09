d02330ae11de975d9127e75b08041d6998a64896
mark <myxfinancials@gmail.com>
2024-07-09 13:53:52 +0800

    revert to system_management/public

--- system_management/app/Helpers/url_helper.php
+++ system_management/app/Helpers/url_helper.php
@@ -1,5 +1,4 @@
 <?php
-
 function getBaseURL() {
     $protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
     $isLocalhost = preg_match('/^localhost(:\d+)?$/', $_SERVER['HTTP_HOST']);
@@ -7,11 +6,10 @@ function getBaseURL() {
     $first_part_of_path = $folder_path[1] ?? '';
     $baseURL = $protocol . "://" . $_SERVER['HTTP_HOST'];
     $baseURL .= "/" . $first_part_of_path;
-    $second_part_of_path = $folder_path[2] ?? '';
-    $baseURL .= "/" . $second_part_of_path;
 
     if (getenv('CI_ENVIRONMENT') === 'development' || $isLocalhost) { 
-
+        $second_part_of_path = $folder_path[2] ?? '';
+        $baseURL .= "/" . $second_part_of_path;
 
         if (!preg_match("/public\/?$/", $baseURL)) {
             $baseURL .= '/public';
@@ -20,7 +18,11 @@ function getBaseURL() {
 
     $baseURL = rtrim($baseURL, '/') . '/';
 
-    if (!preg_match("/system_management\/public\/?$/i", $baseURL)) {
+    // Check if baseURL ends with 'system_management' and not followed by 'public'
+    if (preg_match("/system_management\/?$/i", $baseURL) && !preg_match("/system_management\/public\/?$/i", $baseURL)) {
+        $baseURL .= 'public/';
+    } else if (!preg_match("/system_management\/public\/?$/i", $baseURL)) {
+        // If 'system_management/public' is not already part of the URL, adjust accordingly
         $baseURL = preg_replace("/(public\/?)$/i", "system_management/$1", $baseURL);
     }
 
