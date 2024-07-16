fe26e0363161e43655827500743cae4b8b0fe502
mark <myxfinancials@gmail.com>
2024-07-16 10:39:31 +0800

    OrderSync - verification when first time adding as webhook

--- system_management/app/Controllers/WooCommerceOrderSync/OrderController.php
+++ system_management/app/Controllers/WooCommerceOrderSync/OrderController.php
@@ -16,22 +16,24 @@ class OrderController extends BaseController
         $this->salesOrderModel = new SalesOrderModel();
     }
     public function receiveOrder(){
-        //check if being registered as a webhook in WooCommerce
 
+        // Being able to receive the webhook verification request
+        $webhookId = $this->request->getPost('webhook_id');
 
+        if ($webhookId){
+            return $this->response->setJSON(['message' => 'This is a webhook verification request']);
+        }
+
+        // Verify the webhook signature
         $jsonData = $this->request->getJSON(true);
-
         $webhookSecret = getenv('WEBHOOK_SECRET');
-
         $webhookSignature = $this->request->getHeaderLine('x-wc-webhook-signature');
-
         $computedSignature = base64_encode(hash_hmac('sha256', json_encode($jsonData), $webhookSecret, true));
-
         if (!hash_equals($webhookSignature, $computedSignature)) {
             return $this->response->setJSON(['message' => 'Invalid signature']);
         }
         
-
+        // Insert the order data into the database
         $data = [
             'compcode' => "001",
             'ctranno' => $jsonData['id'],
