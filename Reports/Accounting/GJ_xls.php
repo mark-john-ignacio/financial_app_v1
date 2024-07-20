4a5aa1b9148ec3191472434de1f2e3a0af8bf482
Maita <maita.galang@gmail.com>
2024-07-20 14:27:38 +0800

    debug GJ

--- Reports/Accounting/GJ_xls.php
+++ Reports/Accounting/GJ_xls.php
@@ -39,7 +39,7 @@ $spreadsheet->getProperties()->setCreator('Myx Financials')
 	$sql = "Select A.cmodule, A.ctranno, A.ddate, A.acctno, B.cacctdesc, A.ndebit, A.ncredit, A.ddate, C.cmemo
 			From glactivity A 
 			left join accounts B on A.compcode=B.compcode and A.acctno=B.cacctid
-            left join journal C on A.compcode=B.compcode and A.ctranno=C.ctranno
+            left join journal C on A.compcode=C.compcode and A.ctranno=C.ctranno
 			Where A.compcode='$company' and A.ddate between STR_TO_DATE('$date1', '%m/%d/%Y') and STR_TO_DATE('$date2   ', '%m/%d/%Y')
 			Order By A.dpostdate, A.ctranno, CASE WHEN (A.ndebit <> 0) THEN 1 ELSE 0 END desc, A.acctno";
 
