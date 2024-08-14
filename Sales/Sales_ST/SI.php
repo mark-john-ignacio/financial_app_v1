92934b35caacddc240baa68e32a01cead549da98
Maita <maita.galang@gmail.com>
2024-08-14 10:27:12 +0800

    Sales Updates

--- Sales/Sales_ST/SI.php
+++ Sales/Sales_ST/SI.php
@@ -177,8 +177,9 @@
 			var itmstat = "";
 
 			if(x=="POST"){
+				itmstat = "OK";
 					//generate GL ENtry muna
-					$.ajax ({
+					/*$.ajax ({
 						dataType: "text",
 						url: "../../include/th_toAcc_ST.php",
 						data: { tran: num, type: "SI" },
@@ -192,7 +193,7 @@
 								itmstat = data.trim();	
 							}
 						}
-					});
+					});*/
 					//alert(itmstat);
 					
 					//Send SMS lng
