196aad16cbb9de51f8e60330f9c81f07f847d074
mark <myxfinancials@gmail.com>
2024-08-20 13:58:39 +0800

    BIRForms - added form fields

--- authenticate.php
+++ authenticate.php
@@ -117,7 +117,7 @@
 
         if($GOYes=="True"){
 
-            $stmtlog = $con->prepare("UPDATE users set `session_ID` = ?, `date_last_log` = ?, `machine_last_log` = ?, `mac_last_log` = ? WHERE `userid` = ?");
+            $stmtlog = $con->prepare("UPDATE users set `session_ID` = ?, `date_last_log` = ?, `machine_last_log` = ?, `machine_last_log` = ? WHERE `userid` = ?");
             $stmtlog->bind_param("sssss", $xsessionid, $dateNow, $hashedIP, $hashedMAC, $username);
             $stmtlog->execute();
             $stmtlog->close();
