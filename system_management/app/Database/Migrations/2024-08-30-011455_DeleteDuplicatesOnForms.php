00458d51b83c3632df7315b7d7aad79fe4f6b825
mark <myxfinancials@gmail.com>
2024-08-30 11:54:42 +0800

    Update 2024-08-30-011455_DeleteDuplicatesOnForms.php

--- system_management/app/Database/Migrations/2024-08-30-011455_DeleteDuplicatesOnForms.php
+++ system_management/app/Database/Migrations/2024-08-30-011455_DeleteDuplicatesOnForms.php
@@ -9,15 +9,14 @@ class DeleteDuplicatesOnForms extends Migration
     public function up()
     {
         if ($this->db->tableExists('nav_menu_forms')) {
-            // Identify and remove duplicates
-            $subQuery = $this->db->table('nav_menu_forms')
-                ->select('MIN(id) as id')
-                ->groupBy('form_code')
-                ->getCompiledSelect();
-    
-            $this->db->table('nav_menu_forms')
-                ->where("id NOT IN ($subQuery)", null, false)
-                ->delete();
+            $this->db->query("
+            DELETE FROM nav_menu_forms
+            WHERE id NOT IN (
+                SELECT MIN(id)
+                FROM nav_menu_forms
+                GROUP BY form_code
+            )
+            ");
         }
     }
 
