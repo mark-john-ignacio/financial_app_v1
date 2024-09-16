4ff841e1c9dc6a307be0c03a471f606a8ef7406b
mark <myxfinancials@gmail.com>
2024-09-16 16:32:38 +0800

    Migration of tables for MRP

---
+++ laravel-backend/database/migrations/2024_09_16_082417_create_job_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_number')->unique();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status'); // Posted, For Approval, Approved, Cancelled
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_orders');
+    }
+};
