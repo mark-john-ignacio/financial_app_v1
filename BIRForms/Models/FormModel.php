279284ef5f0e56627aaf8a99488936a9b53949c1
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-16 10:22:34 +0800

    remove BIRForms

--- BIRForms/Models/FormModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-class FormModel {
-    public function isAuthenticated() {
-        session_start();
-        return isset($_SESSION['is_authenticated']) && $_SESSION['is_authenticated'] === true;
-    }
-}
