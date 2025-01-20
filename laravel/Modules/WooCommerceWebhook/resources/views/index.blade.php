0dc1edadd6c013fcd4f2cf52fe5dc7a398f7e82e
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-20 10:22:32 +0800

    move migrations

---
+++ laravel/Modules/WooCommerceWebhook/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('woocommercewebhook::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('woocommercewebhook.name') !!}</p>
+@endsection
---
+++ laravel/Modules/WooCommerceWebhook/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\WooCommerceWebhook\Http\Controllers\WooCommerceWebhookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group([], function () {
+    Route::resource('woocommercewebhook', WooCommerceWebhookController::class)->names('woocommercewebhook');
+});
