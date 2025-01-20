fda729ce49c7b4ac31ca332bd507089d98aa63d2
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-20 10:36:21 +0800

    move seeder

--- laravel/Modules/WooCommerceWebhook/database/seeders/WooCommerceWebhookDatabaseSeeder.php
+++ laravel/Modules/WooCommerceWebhook/database/seeders/WooCommerceWebhookDatabaseSeeder.php
@@ -1,16 +1,57 @@
 <?php
 
-namespace Modules\WooCommerceWebhook\Database\Seeders;
+namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\WooCommerceWebhook\Models\Item;
+use Modules\WooCommerceWebhook\Models\WooCommerceProductMapping;
 
-class WooCommerceWebhookDatabaseSeeder extends Seeder
+class WooCommerceProductMappingSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        // $this->call([]);
+        $items = Item::select('nid')->get();
+
+        foreach ($items as $item){
+            WooCommerceProductMapping::create([
+                'woocommerce_product_id' => null,
+                'myxfin_product_id' => $item->nid
+            ]);
+        }
+
+        $itemA4=Item::where('cpartno', 'ITEMWC001')->first();
+        if(!$itemA4){
+            $itemA4=Item::create([
+                "compcode" => "001",
+                "cpartno" => "ITEMWC0001",
+                "cskucode" => null,
+                "citemdesc" => "A4 TECH OP-720 OPTICAL MOUSE",
+                "cunit" => "PCS",
+                "cclass" => "ICLS0015",
+                "ctype" => "ITEM",
+                "csalestype" => "Goods",
+                "ctradetype" => "Trade",
+                "ctaxcode" => "VT",
+                "cpricetype" => "PM",
+                "nmarkup" => "0.00",
+                "cacctcodesales" => "90",
+                "cacctcodesalescr" => null,
+                "cacctcodewrr" => "102",
+                "cacctcodedr" => null,
+                "cacctcoderet" => "100",
+                "cacctcodecog" => null,
+            ]);
+        }
+
+        WooCommerceProductMapping::create([
+            'woocommerce_product_id' => 11123,
+            'myxfin_product_id' => $itemA4->nid
+        ]);
+
+
     }
 }
