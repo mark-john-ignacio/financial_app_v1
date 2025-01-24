a4930d114e17c1e32df017c03a664dd0ad6be921
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-24 13:53:57 +0800

    Update AppServiceProvider.php

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->booted(function () {
+//            require __DIR__ . '/../../../Connection/connection_string.php';
+//            $prefix = $laravel_prefix;
+//
+//
+//            Livewire::setUpdateRoute(function ($handle) use ($prefix) {
+//                return Route::post($prefix . '/livewire/update', $handle)
+//                    ->name('sanctum.livewire.update')
+//                    ->middleware('web');
+//            });
+//
+//            Livewire::setScriptRoute(function ($handle) use ($prefix) {
+//                return Route::get($prefix . '/livewire/livewire.js', $handle)
+//                    ->name('sanctum.livewire.script')
+//                    ->middleware('web');
+//            });
+//
+//            // Add source maps route
+//            Route::get($prefix . '/livewire/livewire.min.js.map', [
+//                'uses' => '\Livewire\Features\SupportFileUploads\FilePreviewController@maps',
+//                'as' => 'sanctum.livewire.maps'
+//            ])->middleware('web');
+        });
     }
 }
