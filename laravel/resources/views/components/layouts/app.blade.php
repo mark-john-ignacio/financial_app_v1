761a86de82b4ba61fd60d97712238bcc1064a6da
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-25 09:05:46 +0800

    tailwind style working

--- laravel/resources/views/components/layouts/app.blade.php
+++ laravel/resources/views/components/layouts/app.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
-
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
     <body>
