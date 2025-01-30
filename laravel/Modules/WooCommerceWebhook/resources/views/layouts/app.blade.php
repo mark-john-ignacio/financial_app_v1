8673f845caec458aaca944d188a1ce6adfc47deb
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-30 16:33:24 +0800

    reuse cdn

--- laravel/Modules/WooCommerceWebhook/resources/views/layouts/app.blade.php
+++ laravel/Modules/WooCommerceWebhook/resources/views/layouts/app.blade.php
@@ -5,12 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title ?? 'Page Title' }}</title>
 
-        @vite(\Nwidart\Modules\Module::getAssets())
+        <!-- @vite(\Nwidart\Modules\Module::getAssets()) -->
         {{-- DataTables Styles --}}
-        <!-- <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.min.css"> -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.min.css">
 
         {{-- Bootstrap Styles --}}
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
         {{-- Select2 Styles --}}
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
@@ -26,7 +26,7 @@
 
     {{-- JQuery and DataTables Scripts --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- <script src="https://cdn.datatables.net/2.2.1/js/dataTables.min.js"></script> -->
+    <script src="https://cdn.datatables.net/2.2.1/js/dataTables.min.js"></script>
 
     {{-- Bootstrap Scripts --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
