fb2c3b064c46fd7e184e17d396967eb099f51c07
mark <markjohn.ignacio@cvsu.edu.ph>
2025-01-31 14:38:32 +0800

    commented out middlewares for now

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(AuthenticateLegacyUser::class);
+        // $middleware->append(AuthenticateLegacyUser::class);
         // $middleware->append(CheckLegacyUserAccess::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
