0d3ef2fa8b8306e93191c78e864eaf1c718edad8
mark <markjohn.ignacio@cvsu.edu.ph>
2025-02-03 10:12:45 +0800

    put underline way down

--- laravel/Modules/Base/resources/views/components/dropdown-nav-link.blade.php
+++ laravel/Modules/Base/resources/views/components/dropdown-nav-link.blade.php
@@ -2,14 +2,20 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+    ? 'h-16 inline-flex items-center px-1 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none transition duration-150 ease-in-out'
+    : 'h-16 inline-flex items-center px-1 text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 transition duration-150 ease-in-out';
+
+$containerClasses = ($active ?? false)
+    ? 'relative h-full border-b-2 border-indigo-400 dark:border-indigo-600'
+    : 'relative h-full border-b-2 border-transparent hover:border-gray-300 dark:hover:border-gray-700';
 @endphp
 
-<div class="relative h-full flex items-center" x-data="{ open: false }" @click.away="open = false">
+<div class="{{ $containerClasses }}">
     <button type="button" 
-        {{ $attributes->merge(['class' => $classes]) }} 
-        @click="open = !open">
+        {{ $attributes->merge(['class' => $classes]) }}
+        x-data="{ open: false }" 
+        @click="open = !open"
+        @click.away="open = false">
         {{ $trigger }}
     </button>
 
@@ -21,7 +27,7 @@ $classes = ($active ?? false)
          x-transition:leave-start="transform opacity-100 scale-100"
          x-transition:leave-end="transform opacity-0 scale-95"
          class="absolute left-0 z-50 mt-2 w-48 rounded-md shadow-lg origin-top-left"
-         style="display: none; top: 100%;">
+         style="display: none;">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white dark:bg-gray-700">
             {{ $content }}
         </div>
