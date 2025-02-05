64c107cb186555d638fbb90c03aa60c78ddb3a56
mark <markjohn.ignacio@cvsu.edu.ph>
2025-02-05 11:38:39 +0800

    working

--- laravel/tailwind.config.js
+++ laravel/tailwind.config.js
@@ -1,21 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-import preset from './vendor/filament/support/tailwind.config.preset'
 export default {
-    presets: [preset],
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./Modules/**/*.blade.php",
-        "./Modules/**/*.js",
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
-}
+
+    plugins: [forms],
+};
