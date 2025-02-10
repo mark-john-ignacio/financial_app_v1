bfd8db4d85478452894fe47f26db2c368f7b959b
mark <markjohn.ignacio@cvsu.edu.ph>
2025-02-10 08:32:49 +0800

    missing tables on pos_cart

--- system_management/app/Database/Migrations/2025-02-08-085416_AddColumnsToPosCartTable.php
+++ system_management/app/Database/Migrations/2025-02-08-085416_AddColumnsToPosCartTable.php
@@ -8,11 +8,46 @@ class AddColumnsToPosCartTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addColumn('pos_cart', [
+            'item' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'after' => 'status'
+            ],
+            'quantity' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true,
+                'after' => 'item'
+            ],
+            'item_specialDisc' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+                'null' => true,
+                'after' => 'quantity'
+            ],
+            'item_coupon' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+                'null' => true,
+                'after' => 'item_specialDisc'
+            ],
+            'employee_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'after' => 'item_coupon'
+            ]
+        ]);
     }
 
     public function down()
     {
-        //
+        $this->forge->dropColumn('pos_cart', 'item');
+        $this->forge->dropColumn('pos_cart', 'quantity');
+        $this->forge->dropColumn('pos_cart', 'item_specialDisc');
+        $this->forge->dropColumn('pos_cart', 'item_coupon');
+        $this->forge->dropColumn('pos_cart', 'employee_name');
     }
 }
